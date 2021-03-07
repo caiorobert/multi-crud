c4ca62d62e2298107c056dff97d669bd390043f8
Caio Robert <ccruz@viceri.com.br>
2021-03-07 16:35:52 -0300

    Curso Viceri com Kenerry sobre criação de API

---
+++ MultiCrud/MultiCrud.Infraestructure.Repository/Repositories/EntityRepositoryBase.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiCrud.Domain.Abstractions.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MultiCrud.Infrastructure.Repository.Repositories
+{
+    public abstract class EntityRepositoryBase<TEntity>: IEntityRepositoryBase<TEntity> where TEntity : class
+    {
+        private readonly DbSet<TEntity> _entityDbSet;
+        public EntityRepositoryBase(DbSet<TEntity> entityDbSet)
+        {
+            _entityDbSet = entityDbSet ?? throw new ArgumentNullException(nameof(entityDbSet));
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync
+        (
+            int skip,
+            int take,
+            Expression<Func<TEntity, bool>> where,
+            Expression<Func<TEntity, object>> orderBy
+        )
+        {
+            return await _entityDbSet
+                .AsNoTracking()
+                .OrderBy(orderBy)
+                .Where(where)
+                .Skip(skip)
+                .Take(take)
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(int skip,int take)
+        {
+            return await _entityDbSet
+                .AsNoTracking()
+                .Skip(skip)
+                .Take(take)
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _entityDbSet
+                .AsNoTracking()
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+
+        public async Task<TEntity> GetByIdAsync(int key)
+        {
+            return await _entityDbSet.FindAsync(key).ConfigureAwait(false);
+        }
+
+        public async Task InsertAsync(TEntity entity)
+        {
+            await _entityDbSet.AddAsync(entity).ConfigureAwait(false);
+        }
+
+        public Task UpdateAsync(TEntity entity)
+        {
+            _entityDbSet.Update(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task RemoveAsync(int key)
+        {
+            var entityToRemove = _entityDbSet.Find(key);
+            _entityDbSet.Remove(entityToRemove);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ MultiCrud/MultiCrud.Infraestructure.Repository/Repositories/PersonEntityRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiCrud.Domain.Abstractions.Repositories;
+using MultiCrud.Domain.Entities;
+
+namespace MultiCrud.Infrastructure.Repository.Repositories
+{
+    public sealed class PersonEntityRepository : EntityRepositoryBase<Person>, IPersonEntityRepository
+    {
+        public PersonEntityRepository(DbSet<Person> entityDbSet) : base(entityDbSet)
+        {
+        }
+    }
+}
